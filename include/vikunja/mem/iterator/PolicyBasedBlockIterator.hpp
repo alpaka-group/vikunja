6c3d8f6c8b6232aea9adec38e6e29b6a0e23b11c
Hauke Mewes <haukemewes@gmail.com>
2019-01-16 14:20:02 +0100

    added policy based block iterator which can be used for grid striding access in block reduce like algorithms

---
+++ include/vikunja/mem/iterator/PolicyBasedBlockIterator.hpp
@@ -0,0 +1,204 @@
+//
+// Created by mewes30 on 16.01.19.
+//
+
+#pragma once
+
+#include "BaseIterator.hpp"
+#include <alpaka/alpaka.hpp>
+
+namespace vikunja {
+    namespace mem {
+        namespace iterator {
+            template <typename MemAccessPolicy, typename TAcc, typename T, typename TBuf = T>
+            class PolicyBasedBlockIterator : public BaseIterator<T, TBuf> {
+            private:
+                uint64_t mStep;
+            public:
+                
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE PolicyBasedBlockIterator(const TBuf * data, TAcc const &acc, uint64_t problemSize, uint64_t blockSize) : BaseIterator<T, TBuf>(data, MemAccessPolicy::getStartIndex(acc, problemSize, blockSize), MemAccessPolicy::getEndIndex(acc, problemSize, blockSize)), mStep(MemAccessPolicy::getStepSize(acc, problemSize, blockSize))
+                {}
+
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE PolicyBasedBlockIterator(const PolicyBasedBlockIterator &other) = default;
+                
+                //-----------------------------------------------------------------------------
+                //! Returns the iterator for the last item.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto end() const -> PolicyBasedBlockIterator
+                {
+                    PolicyBasedBlockIterator ret(*this);
+                    ret.mIndex = this->mMaximum;
+                    return ret;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Increments the internal pointer to the next one and returns this
+                //! element.
+                //!
+                //! Returns a reference to the next index.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator++() -> PolicyBasedBlockIterator &
+                {
+                    this->mIndex += this->mStep;
+                    return *this;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Returns the current element and increments the internal pointer to the
+                //! next one.
+                //!
+                //! Returns a reference to the current index.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator++(int) -> PolicyBasedBlockIterator
+                {
+                    auto ret(*this);
+                    this->mIndex += this->mStep;
+                    return ret;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Decrements the internal pointer to the previous one and returns the this
+                //! element.
+                //!
+                //! Returns a reference to the previous index.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator--() -> PolicyBasedBlockIterator &
+                {
+                    this->mIndex -= this->mStep;
+                    return *this;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Returns the current element and decrements the internal pointer to the
+                //! previous one.
+                //!
+                //! Returns a reference to the current index.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator--(int) -> PolicyBasedBlockIterator
+                {
+                    auto ret(*this);
+                    this->mIndex -= this->mStep;
+                    return ret;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Returns the index + a supplied offset.
+                //!
+                //! \param n The offset.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator+(uint64_t n) const
+                -> PolicyBasedBlockIterator
+                {
+                    auto ret(*this);
+                    ret.mIndex += n * mStep;
+                    return ret;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Returns the index - a supplied offset.
+                //!
+                //! \param n The offset.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator-(uint64_t n) const
+                -> PolicyBasedBlockIterator
+                {
+                    auto ret(*this);
+                    ret.mIndex -= n * mStep;
+                    return ret;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Addition assignment.
+                //!
+                //! \param offset The offset.
+                //!
+                //! Returns the current object offset by the offset.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator+=(uint64_t offset)
+                -> PolicyBasedBlockIterator &
+                {
+                    this->mIndex += offset * this->mStep;
+                    return *this;
+                }
+
+                //-----------------------------------------------------------------------------
+                //! Substraction assignment.
+                //!
+                //! \param offset The offset.
+                //!
+                //! Returns the current object offset by the offset.
+                ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE auto operator-=(uint64_t offset)
+                -> PolicyBasedBlockIterator &
+                {
+                    this->mIndex -= offset * this->mStep;
+                    return *this;
+                }
+            };
+            
+            namespace policies {
+                struct LinearMemAccessPolicy {
+                    template<typename TAcc, typename TIdx>
+                    ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE
+                    static auto getStartIndex(TAcc const &acc, TIdx const &problemSize, TIdx const &blockSize) -> TIdx const {
+                        auto gridDimension{alpaka::workdiv::getWorkDiv<alpaka::Grid, alpaka::Blocks>(acc)[0]};
+                        auto indexInBlock{alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0]};
+                        auto gridSize = gridDimension * blockSize;
+                        // TODO: catch overflow
+                        return (problemSize * indexInBlock) / gridSize;
+                    }
+
+                    template<typename TAcc, typename TIdx>
+                    ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE
+                    static auto getEndIndex(TAcc const &acc, TIdx const &problemSize, TIdx const &blockSize) -> TIdx const {
+                        auto gridDimension{alpaka::workdiv::getWorkDiv<alpaka::Grid, alpaka::Blocks>(acc)[0]};
+                        auto indexInBlock{alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0]};
+                        auto gridSize = gridDimension * blockSize;
+                        // TODO: catch overflow
+                        return (problemSize * indexInBlock + problemSize) / gridSize;
+                    }
+
+                    template<typename TAcc, typename TIdx>
+                    ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE
+                    static constexpr auto getStepSize(TAcc const &acc, TIdx const &problemSize, TIdx const &blockSize) -> TIdx const {
+                        return 1;
+                    }
+                };
+
+                struct GridStridingMemAccessPolicy {
+                    template<typename TAcc, typename TIdx>
+                    ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE
+                    static auto getStartIndex(TAcc const &acc, TIdx const &problemSize, TIdx const &blockSize) -> TIdx const {
+                        return alpaka::idx::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0];
+                    }
+
+                    template<typename TAcc, typename TIdx>
+                    ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE
+                    static auto getEndIndex(TAcc const &acc, TIdx const &problemSize, TIdx const &blockSize) -> TIdx const {
+                        return problemSize;
+                    }
+
+                    template<typename TAcc, typename TIdx>
+                    ALPAKA_FN_HOST_ACC ALPAKA_FN_INLINE
+                    static auto getStepSize(TAcc const &acc, TIdx const &problemSize, TIdx const &blockSize) {
+                        auto gridDimension{alpaka::workdiv::getWorkDiv<alpaka::Grid, alpaka::Blocks>(acc)[0]};
+                        return gridDimension * blockSize;
+                    }
+                };
+            } // policies
+
+            namespace traits {
+                template<typename TAcc, typename TSfinae = void>
+                struct GetMemAccessPolicyByPltf{};
+
+                template<>
+                struct GetMemAccessPolicyByPltf<alpaka::pltf::PltfCpu> {
+                    using type = policies::LinearMemAccessPolicy;
+                };
+
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+                template<>
+                struct GetMemAccessPolicyByPltf<alpaka::pltf::PltfCudaRt> {
+                    using type = policies::GridStridingMemAccessPolicy;
+                };
+#endif // ALPAKA_ACC_GPU_CUDA_ENABLED
+
+            } //traits
+
+            // shortcut to derive policy from accelerator
+            template<typename TAcc>
+            using MemAccessPolicy = typename traits::GetMemAccessPolicyByPltf<alpaka::pltf::Pltf<alpaka::dev::Dev<TAcc>>>::type;
+        } // iterator
+    } // mem
+} // vikunja
--- test/unit/iterator/src/Iterator.cpp
+++ test/unit/iterator/src/Iterator.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <vikunja/mem/iterator/BaseIterator.hpp>
+#include <vikunja/mem/iterator/PolicyBasedBlockIterator.hpp>
 #include <alpaka/alpaka.hpp>
 #include <catch2/catch.hpp>
 #include <vector>
@@ -18,7 +19,6 @@ std::vector<Type> generateIndexVector(Idx size) {
     }
     return data;
 }
-
 
 TEST_CASE("BaseIterator", "[iterator]") {
 
@@ -46,4 +46,67 @@ TEST_CASE("BaseIterator", "[iterator]") {
     REQUIRE(*one == 1);
     REQUIRE(copyOfZeroFirst == zeroFirst);
 
+};
+
+template<typename MemAccessPolicy>
+struct TestPolicyBasedBlockIteratorKernel {
+};
+
+template<>
+struct TestPolicyBasedBlockIteratorKernel<vikunja::mem::iterator::policies::LinearMemAccessPolicy> {
+    template<typename TAcc, typename TIdx>
+    ALPAKA_FN_ACC void operator()(TAcc const &acc,
+                                  Type *data, TIdx const &n) const {
+        
+    }
+};
+
+template <typename TAcc>
+struct TestPolicyBasedBlockIterator {
+    template <typename MemAccessPolicy>
+    void operator()() {
+        using Idx = alpaka::idx::Idx<TAcc>;
+        using Dim = alpaka::dim::Dim<TAcc>;
+
+        constexpr Idx n = 65;
+        constexpr Idx blocksPerGrid = 2;
+        constexpr Idx threadsPerBlock = 1;
+        constexpr Idx elementsPerThread = n / 2 + 1;
+
+        using DevAcc = alpaka::dev::Dev<TAcc>;
+        using PltfAcc = alpaka::pltf::Pltf<DevAcc>;
+        using QueueAcc = alpaka::queue::QueueCpuSync;
+        //alpaka::test::queue::DefaultQueue<alpaka::dev::Dev<TAcc>>;
+        using PltfHost = alpaka::pltf::PltfCpu;
+        using DevHost = alpaka::dev::Dev<PltfHost>;
+        using QueueHost = alpaka::queue::QueueCpuSync;
+        using WorkDiv = alpaka::workdiv::WorkDivMembers<Dim, Idx>;
+        // Get the host device.
+        DevHost devHost(
+                alpaka::pltf::getDevByIdx<PltfHost>(0u));
+        // Get a queue on the host device.
+        QueueHost queueHost(
+                devHost);
+        // Select a device to execute on.
+        DevAcc devAcc(
+                alpaka::pltf::getDevByIdx<PltfAcc>(0u));
+        // Get a queue on the accelerator device.
+        QueueAcc queueAcc(
+                devAcc);
+        WorkDiv workdiv{
+                blocksPerGrid,
+                threadsPerBlock,
+                elementsPerThread
+        };
+
+    }
+};
+
+TEST_CASE("PolicyBasedBlockIterator", "[iterator]") {
+
+    constexpr Idx size = 64;
+
+    SECTION("LinearMemAccessPolicy") {
+
+    }
 }
