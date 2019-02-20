32fa106be5ccbbf2d9759ff63ade30fa5712aedc
Hauke Mewes <haukemewes@gmail.com>
2019-02-20 16:13:27 +0100

    added two-iterator transform

--- include/vikunja/transform/detail/BlockThreadTransformKernel.hpp
+++ include/vikunja/transform/detail/BlockThreadTransformKernel.hpp
@@ -30,6 +30,26 @@ namespace vikunja {
                         ++outputIterator;
                     }
                 }
+
+                template<typename TAcc, typename TIdx,
+                        typename TInputIterator, typename TInputIteratorSecond, typename TOutputIterator, typename TFunc>
+                ALPAKA_FN_ACC void operator()(TAcc const &acc,
+                                              TInputIterator const &source,
+                                              TInputIteratorSecond const &sourceSecond,
+                                              TOutputIterator const &destination,
+                                              TIdx const &n,
+                                              TFunc const &func) const {
+                    vikunja::mem::iterator::PolicyBasedBlockIterator<TMemAccessPolicy, TAcc, TInputIterator> inputIterator(source, acc, n, TBlockSize);
+                    vikunja::mem::iterator::PolicyBasedBlockIterator<TMemAccessPolicy, TAcc, TInputIteratorSecond> inputIteratorSecond(sourceSecond, acc, n, TBlockSize);
+                    vikunja::mem::iterator::PolicyBasedBlockIterator<TMemAccessPolicy, TAcc, TOutputIterator> outputIterator(destination, acc, n, TBlockSize);
+
+                    while(inputIterator < inputIterator.end()) {
+                        *outputIterator = func(*inputIterator, *inputIteratorSecond);
+                        ++inputIterator;
+                        ++inputIteratorSecond;
+                        ++outputIterator;
+                    }
+                }
             };
         }
     }
--- include/vikunja/transform/transform.hpp
+++ include/vikunja/transform/transform.hpp
@@ -41,5 +41,33 @@ namespace vikunja {
             detail::BlockThreadTransformKernel<blockSize, MemAccessPolicy> kernel;
             alpaka::kernel::exec<TAcc>(queue, multiBlockWorkDiv, kernel, source, destination, n, func);
         }
+
+        template<typename TAcc, typename WorkDivPolicy = vikunja::workdiv::BlockBasedPolicy<TAcc>, typename MemAccessPolicy = vikunja::mem::iterator::MemAccessPolicy<TAcc>, typename TFunc, typename TInputIterator, typename TInputIteratorSecond, typename TOutputIterator, typename TDevAcc, typename TQueue, typename TIdx >
+        auto deviceTransform(TDevAcc &devAcc, TQueue &queue,  TIdx const &n, TInputIterator const &source, TInputIteratorSecond const &sourceSecond, TOutputIterator const &destination, TFunc const &func) -> void {
+            if(n == 0) {
+                return;
+            }
+            constexpr uint64_t blockSize = WorkDivPolicy::template getBlockSize<TAcc>();
+            using Dim = alpaka::dim::Dim<TAcc>;
+            using WorkDiv = alpaka::workdiv::WorkDivMembers<Dim, TIdx>;
+            if(n < blockSize) {
+                // TODO fix this?
+                // maybe not needed
+            }
+            TIdx gridSize = WorkDivPolicy::template getGridSize<TAcc>(devAcc);
+            TIdx maxGridSize = static_cast<TIdx>(
+                    (((n + 1) / 2) - 1) / static_cast<TIdx>(blockSize) + 1);
+            if(gridSize > maxGridSize) {
+                gridSize = maxGridSize;
+            }
+            TIdx workDivGridSize = gridSize;
+            TIdx workDivBlockSize = blockSize;
+
+            WorkDiv multiBlockWorkDiv{ static_cast<TIdx>(workDivGridSize),
+                                       static_cast<TIdx>(workDivBlockSize),
+                                       static_cast<TIdx>(1) };
+            detail::BlockThreadTransformKernel<blockSize, MemAccessPolicy> kernel;
+            alpaka::kernel::exec<TAcc>(queue, multiBlockWorkDiv, kernel, source, sourceSecond, destination, n, func);
+        }
     } // transform
 } // vikunja
