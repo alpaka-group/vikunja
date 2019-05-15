d8636b83ef4a6eada1f4e0ea9c2d6c5267ab4be6
Hauke Mewes <haukemewes@gmail.com>
2019-05-15 16:28:56 +0200

    minor change in unit test

--- test/integ/transform/src/Transform.cpp
+++ test/integ/transform/src/Transform.cpp
@@ -27,6 +27,69 @@ struct incOne {
     }
 };
 
+template<typename TAcc>
+struct TestAlpakaEnv {
+public:
+    using Idx = alpaka::idx::Idx<TAcc>;
+    using Dim = alpaka::dim::Dim<TAcc>;
+    using Vec = alpaka::vec::Vec<Dim, Idx>;
+    using DevAcc = alpaka::dev::Dev<TAcc>;
+    using PltfAcc = alpaka::pltf::Pltf<DevAcc>;
+    using PltfHost = alpaka::pltf::PltfCpu;
+    using DevHost = alpaka::dev::Dev<PltfHost>;
+    using QueueAcc = //alpaka::queue::QueueCpuAsync;*/
+    typename std::conditional<std::is_same<PltfAcc, alpaka::pltf::PltfCpu>::value, alpaka::queue::QueueCpuSync,
+#ifdef  ALPAKA_ACC_GPU_CUDA_ENABLED
+            alpaka::queue::QueueCudaRtSync
+#else
+            alpaka::queue::QueueCpuSync
+#endif
+    >::type;
+
+    static constexpr Idx xIndex = Dim::value - 1u;
+
+    DevAcc acc;
+    DevHost host;
+    QueueAcc queue;
+
+    TestAlpakaEnv() :
+    acc(alpaka::pltf::getDevByIdx<PltfAcc>(0u)),
+    host(alpaka::pltf::getDevByIdx<PltfHost>(0u)),
+    queue(acc) {
+
+    }
+
+    Vec getSingleDimensionVec(Idx const val) {
+        Vec vec(Vec::all(static_cast<Idx>(1)));
+        vec[xIndex] = val;
+        return vec;
+    }
+
+    template<typename TValue>
+    auto allocHost(Vec const &extent) {
+        return alpaka::mem::buf::alloc<TValue, Idx>(host, extent);
+    }
+    template<typename TValue>
+    auto allocAcc(Vec const &extent) {
+        return alpaka::mem::buf::alloc<TValue, Idx>(acc, extent);
+    }
+
+    template<typename TLambda>
+    auto fillDeviceBuffer(Vec const &extent, TLambda const &lambda) {
+        using TValue = decltype(lambda(static_cast<Idx>(0u)));
+        auto deviceMem(allocAcc<TValue>(extent));
+        auto hostMem(allocHost<TValue>(extent));
+        auto hostPtr = alpaka::mem::view::getPtrNative(hostMem);
+        const auto n = extent[xIndex];
+        for(Idx i = 0; i < n; ++i) {
+            hostPtr[i] = lambda(i);
+        }
+        alpaka::mem::view::copy(queue, deviceMem, hostMem, extent);
+        return deviceMem;
+    }
+
+};
+
 
 struct TestTemplate {
 private:
@@ -39,7 +102,7 @@ public:
     void operator()() {
         using TRed = uint64_t;
 
-        using Idx = alpaka::idx::Idx<TAcc>;
+        /*using Idx = alpaka::idx::Idx<TAcc>;
         using Dim = alpaka::dim::Dim<TAcc>;
         const Idx n = static_cast<Idx>(memSize);
         constexpr Idx blocksPerGrid = 8;
@@ -49,16 +112,8 @@ public:
         using Vec = alpaka::vec::Vec<Dim, Idx>;
         constexpr Idx xIndex = Dim::value - 1u;
 
-        Vec gridSize(Vec::all(static_cast<Idx>(1u)));
-        Vec blockSize(Vec::all(static_cast<Idx>(1u)));
-        Vec threadSize(Vec::all(static_cast<Idx>(1u)));
-
         Vec extent(Vec::all(static_cast<Idx>(1)));
         extent[xIndex] = n;
-
-        gridSize[xIndex] = blocksPerGrid;
-        blockSize[xIndex] = threadsPerBlock;
-        threadSize[xIndex] = elementsPerThread;
 
         using DevAcc = alpaka::dev::Dev<TAcc>;
         using PltfAcc = alpaka::pltf::Pltf<DevAcc>;
@@ -73,38 +128,20 @@ public:
 #else
                 alpaka::queue::QueueCpuSync
 #endif
-        >::type;
-        using QueueHost = alpaka::queue::QueueCpuSync;
-        using WorkDiv = alpaka::workdiv::WorkDivMembers<Dim, Idx>;
+        >::type;*/
+        TestAlpakaEnv<TAcc> testAlpakaEnv;
+        using Idx = typename TestAlpakaEnv<TAcc>::Idx;
+        const Idx n = memSize;
+        //auto &devHost = testAlpakaEnv.host;
+        auto &devAcc = testAlpakaEnv.acc;
+        auto &queueAcc = testAlpakaEnv.queue;
+        auto extent = testAlpakaEnv.getSingleDimensionVec(memSize);
 
-        // Get the host device.
-        DevHost devHost(
-                alpaka::pltf::getDevByIdx<PltfHost>(0u));
-        // Get a queue on the host device.
-        QueueHost queueHost(
-                devHost);
-        // Select a device to execute on.
-        DevAcc devAcc(
-                alpaka::pltf::getDevByIdx<PltfAcc>(0u));
-        // Get a queue on the accelerator device.
-        QueueAcc queueAcc(
-                devAcc);
-        WorkDiv workdiv{
-                gridSize,
-                blockSize,
-                threadSize
-        };
-
-        auto deviceMem(alpaka::mem::buf::alloc<TRed, Idx>(devAcc, extent));
-        auto hostMem(alpaka::mem::buf::alloc<TRed, Idx>(devHost, extent));
-        uint64_t* hostNative = alpaka::mem::view::getPtrNative(hostMem);
-        for(Idx i = 0; i < memSize; ++i) {
-            hostNative[i] = i + 1;
-        }
-        alpaka::mem::view::copy(queueAcc, deviceMem, hostMem, extent);
         auto incrementOne = [=] ALPAKA_FN_HOST_ACC (Idx i) {
             return i + 1;
         };
+        auto deviceMem(testAlpakaEnv.fillDeviceBuffer(extent, incrementOne));
+        auto hostMem(testAlpakaEnv.template allocHost<TRed>(extent));
 
         std::cout << "Testing accelerator: " << alpaka::acc::getAccName<TAcc>() << " with size: " << n <<"\n";
 
