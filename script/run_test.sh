a4cf858d333d677d13f81f9720c5fdb995bdb9e5
Simeon Ehrig <s.ehrig@hzdr.de>
2021-07-14 11:20:13 +0000

    extend CI to test different alpaka versions
    - update CI container version

--- script/run_test.sh
+++ script/run_test.sh
@@ -31,30 +31,40 @@ done
 # build an run tests
 ###################################################
 
-# install alpaka
-git clone --depth 1 --branch 0.6.0 https://github.com/alpaka-group/alpaka.git
-mkdir alpaka/build && cd alpaka/build
-cmake .. -DBOOST_ROOT=/opt/boost/1.73.0
-cmake --install .
-cd ../..
-rm -rf alpaka
+base_dir=$(pwd)
 
-# use one build directory for all build configurations
-mkdir build
-cd build
+for ALPAKA_VERSION in ${VIKUNJA_ALPAKA_VERSIONS}; do
+    cd $base_dir
+    # install alpaka
+    git clone --depth 1 --branch ${ALPAKA_VERSION} https://github.com/alpaka-group/alpaka.git
+    mkdir alpaka/build && cd alpaka/build
+    cmake .. -DBOOST_ROOT=/opt/boost/1.73.0
+    cmake --install .
+    cd ../..
+    rm -rf alpaka
 
-# ALPAKA_ACCS contains the backends, which are used for each build
-# the backends are set in the sepcialized base jobs .base_gcc,.base_clang and.base_cuda
-for CONFIG in $(seq 0 $((${#CMAKE_CONFIGS[*]} - 1))); do
-    CMAKE_ARGS=${CMAKE_CONFIGS[$CONFIG]}
-    echo -e "\033[0;32m///////////////////////////////////////////////////"
-    echo "number of processor threads -> $(nproc)"
-    cmake --version | head -n 1
-    echo "CMAKE_ARGS -> ${CMAKE_ARGS}"
-    echo -e "/////////////////////////////////////////////////// \033[0m \n\n"
+    # use one build directory for all build configurations
+    mkdir -p build
+    cd build
 
-    cmake .. $CMAKE_ARGS
-    cmake --build . -j
-    ctest
-    rm -r *
+    # ALPAKA_ACCS contains the backends, which are used for each build
+    # the backends are set in the sepcialized base jobs .base_gcc,.base_clang and.base_cuda
+    for CONFIG in $(seq 0 $((${#CMAKE_CONFIGS[*]} - 1))); do
+	CMAKE_ARGS=${CMAKE_CONFIGS[$CONFIG]}
+	echo -e "\033[0;32m///////////////////////////////////////////////////"
+	echo "number of processor threads -> $(nproc)"
+	echo "ALPAKA_VERSION -> ${ALPAKA_VERSION}"
+	cmake --version | head -n 1
+	echo "CMAKE_ARGS -> ${CMAKE_ARGS}"
+	echo -e "/////////////////////////////////////////////////// \033[0m \n\n"
+
+	cmake .. $CMAKE_ARGS
+	cmake --build . -j
+	ctest
+	rm -r *
+    done
+
+    # uninstall alpaka
+    rm -r /usr/local/include/alpaka
+    rm -r /usr/local/lib/cmake/alpaka
 done
