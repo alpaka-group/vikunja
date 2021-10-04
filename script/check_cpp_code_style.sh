e401eee884759bfe2cec18d1db13af1f4b3d9c9c
Simeon Ehrig <s.ehrig@hzdr.de>
2021-10-04 13:59:04 +0000

    Add missing license headers in the source files

--- script/check_cpp_code_style.sh
+++ script/check_cpp_code_style.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Copyright 2021 René Widera
+#
+# This file is part of vikunja.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 set -e
 set -o pipefail
 
--- script/run_test.sh
+++ script/run_test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Copyright 2021 Simeon Ehrig
+#
+# This file is part of vikunja.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 echo "container version -> $CONTAINER_VERSION"
 
 : ${VIKUNJA_CXX?"VIKUNJA_CXX must be specified"}
