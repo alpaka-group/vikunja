e766eb6131e1f78d0e8d80719d4d40ed9c64d279
Simeon Ehrig <s.ehrig@hzdr.de>
2022-04-26 09:06:06 +0000

    restructure folder containing CI related scripts
    - remove temporary normal CI test jobs

--- ci/gitlab_scripts/integration_test/prepare_find_package.sh
+++ ci/gitlab_scripts/integration_test/prepare_find_package.sh
@@ -14,6 +14,49 @@ set -o pipefail
 echo "CUPLA_TRANSFORM_DIR -> ${CUPLA_TRANSFORM_DIR}"
 
 ###############################################
+# Install alpaka
+###############################################
+
+cd $CI_PROJECT_DIR
+git clone https://github.com/alpaka-group/alpaka.git
+cd alpaka
+# use this specific alpaka version because of bug fixes for alpaka_add_library
+git checkout 261bdf70f359b3d97dfdfb3cc2bd39ec0472c8d1
+mkdir build && cd build
+cmake .. -DBOOST_ROOT=/opt/boost/${VIKUNJA_BOOST_VERSIONS}
+cmake --build .
+cmake --install .
+cd $CI_PROJECT_DIR
+rm -r alpaka
+
+###############################################
+# Install cupla
+###############################################
+
+cd $CI_PROJECT_DIR
+git clone https://github.com/alpaka-group/cupla.git
+cd cupla
+# use this specific cupla version because of a bug fix in the CMakeLists.txt
+git checkout d83fe957009e7b3774f423b3f53887a7af50aabe
+mkdir build && cd build
+cmake .. -DBOOST_ROOT=/opt/boost/${VIKUNJA_BOOST_VERSIONS}
+cmake --build .
+cmake --install .
+cd $CI_PROJECT_DIR
+rm -r cupla
+
+###############################################
+# install vikunja
+###############################################
+
+cd $CI_PROJECT_DIR
+mkdir build && cd build
+cmake .. -DBOOST_ROOT=/opt/boost/${VIKUNJA_BOOST_VERSIONS}
+cmake --build .
+cmake --install .
+cd $CI_PROJECT_DIR
+
+###############################################
 # prepare project
 ###############################################
 
@@ -21,36 +64,7 @@ mkdir $CUPLA_TRANSFORM_DIR
 cd $CUPLA_TRANSFORM_DIR
 cp -r ../example/cuplaReduce/* .
 rm CMakeLists.txt
-cp ../script/integration_test/addsubdirectory.cmake CMakeLists.txt
-
-###############################################
-# add alpaka
-###############################################
-
-cd $CUPLA_TRANSFORM_DIR
-git clone https://github.com/alpaka-group/alpaka.git
-cd alpaka
-# use this specific alpaka version because of bug fixes for alpaka_add_library
-git checkout 261bdf70f359b3d97dfdfb3cc2bd39ec0472c8d1
-cd ..
-
-###############################################
-# add cupla
-###############################################
-
-cd $CUPLA_TRANSFORM_DIR
-git clone https://github.com/alpaka-group/cupla.git
-cd cupla
-# use this specific cupla version because of a bug fix in the CMakeLists.txt
-git checkout d83fe957009e7b3774f423b3f53887a7af50aabe
-cd ..
-
-###############################################
-# add vikunja
-###############################################
-
-cd $CUPLA_TRANSFORM_DIR
-ln -s ../../vikunja .
+cp ../ci/gitlab_scripts/integration_test/findPackage.cmake CMakeLists.txt
 
 ls
 cat CMakeLists.txt
