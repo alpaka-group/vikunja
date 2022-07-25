abe8681bf0d72968cf91902e0d7ffb73994c6f5d
Simeon Ehrig <s.ehrig@hzdr.de>
2022-07-25 09:08:58 +0000

    Add function to filter and reorder CI jobs
    This commit message demonstrate, how it work. The job filter removes all jobs, that names does not start with NVCC or GCC. After that, the jobs are reorder. First all GCC11 will be executed, than all GCC8 and then the rest.
    
    CI_FILTER: ^NVCC|^GCC
    CI_REORDER: ^GCC11 ^GCC8

--- ci/job_generator/job_generator.py
+++ ci/job_generator/job_generator.py
@@ -1,11 +1,12 @@
 """Generate GitLab-CI test jobs yaml for the vikunja CI."""
 import argparse
-import sys
+import sys, os
 from typing import List, Dict, Tuple
 from collections import OrderedDict
 
 import alpaka_job_coverage as ajc
 from alpaka_job_coverage.globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from alpaka_job_coverage.util import filter_job_list, reorder_job_list
 
 from vikunja_globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
 from versions import (
@@ -58,6 +59,22 @@ def get_args() -> argparse.Namespace:
         help="Path of the generated jobs yaml.",
     )
 
+    parser.add_argument(
+        "--filter",
+        type=str,
+        default="",
+        help="Filter the jobs with a Python regex that checks the job names.",
+    )
+
+    parser.add_argument(
+        "--reorder",
+        type=str,
+        default="",
+        help="Orders jobs by their names. Expects a string consisting of one or more Python regex. "
+        'The regex are separated by whitespaces. For example, the regex "^NVCC ^GCC" has the '
+        "behavior that all NVCC jobs are executed first and then all GCC jobs.",
+    )
+
     return parser.parse_args()
 
 
@@ -103,6 +120,31 @@ if __name__ == "__main__":
         job_matrix=job_matrix, container_version=args.version
     )
     add_custom_jobs(job_matrix_yaml, args.version)
+
+    filter_regix = args.filter
+    reorder_regix = args.reorder
+
+    COMMIT_MESSAGE_FILTER_PREFIX = "CI_FILTER:"
+    COMMIT_MESSAGE_REORDER_PREFIX = "CI_REORDER:"
+
+    # If the environment variable CI_COMMIT_MESSAGE exists (like in GitLabCI Job),
+    # check for the prefixes and overwrite argument values of --filter and --reorder if
+    # prefix was found in the beginning of a line.
+    if os.getenv("CI_COMMIT_MESSAGE"):
+        for line in os.getenv("CI_COMMIT_MESSAGE").split("\n"):
+            striped_line = line.strip()
+            if striped_line.strip().startswith(COMMIT_MESSAGE_FILTER_PREFIX):
+                filter_regix = striped_line[len(COMMIT_MESSAGE_FILTER_PREFIX) :].strip()
+            if striped_line.startswith(COMMIT_MESSAGE_REORDER_PREFIX):
+                reorder_regix = striped_line[
+                    len(COMMIT_MESSAGE_REORDER_PREFIX) :
+                ].strip()
+
+    if filter_regix:
+        job_matrix_yaml = filter_job_list(job_matrix_yaml, filter_regix)
+
+    if reorder_regix:
+        job_matrix_yaml = reorder_job_list(job_matrix_yaml, reorder_regix)
 
     wave_job_matrix = ajc.distribute_to_waves(job_matrix_yaml, 10)
 
